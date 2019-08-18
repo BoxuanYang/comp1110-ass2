96858bc0fd833e0e207bbf330640b1d028cad0e4
nicole <u6947648@anu.edu.au>
2019-08-19 09:11:18 +1000

    -Enabled Upstream pulls -Parts of the project structure we created in the first meeting

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -8,6 +8,17 @@ public class Board extends Application {
     private static final int BOARD_WIDTH = 933;
     private static final int BOARD_HEIGHT = 700;
 
+    public boolean isLocationValid() {
+        return false;
+    }
+
+    public boolean isLocationEmpty() {
+        return false;
+    }
+
+    public String rotatePiece() {
+        return "";
+    }
     // FIXME Task 7: Implement a basic playable Focus Game in JavaFX that only allows pieces to be placed in valid places
 
     // FIXME Task 8: Implement challenges (you may use challenges and assets provided for you in comp1110.ass2.gui.assets: sq-b.png, sq-g.png, sq-r.png & sq-w.png)
