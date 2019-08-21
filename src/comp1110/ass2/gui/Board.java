842dd431fac81c2ebe7672442ce682dae73269fc
nicole <u6947648@anu.edu.au>
2019-08-21 11:20:02 +1000

    Skeleton for Task 7 and 8

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -8,22 +8,44 @@ public class Board extends Application {
     private static final int BOARD_WIDTH = 933;
     private static final int BOARD_HEIGHT = 700;
 
+
+    // FIXME Task 7: Implement a basic playable Focus Game in JavaFX that only allows pieces to be placed in valid places
+
+    // TASK 7: checks if location is on the board
     public boolean isLocationValid() {
         return false;
     }
 
+    // TASK 7: checks if location does not have a tile already on the board in that place
     public boolean isLocationEmpty() {
         return false;
     }
 
-    public String rotatePiece() {
-        return "";
+    // TASK 7: Adds piece to the board if isLocationEmpty and isLocationValid are both true
+    public void addPiece() {
+
     }
-    // FIXME Task 7: Implement a basic playable Focus Game in JavaFX that only allows pieces to be placed in valid places
 
     // FIXME Task 8: Implement challenges (you may use challenges and assets provided for you in comp1110.ass2.gui.assets: sq-b.png, sq-g.png, sq-r.png & sq-w.png)
 
+    // TASK 8: get a starting placement from SOLUTIONS in TestUtility.Solution
+    public String startPlacement() {
+        return "";
+    }
+
+    // TASK 8: add the startPlacement to the board
+    public void placeStart() {
+    }
+
+    // TASK 8: return a solution for the game
+    public String gameSolution() {
+        return "";
+    }
+
+
     // FIXME Task 10: Implement hints
+
+
 
     // FIXME Task 11: Generate interesting challenges (each challenge may have just one solution)
 
