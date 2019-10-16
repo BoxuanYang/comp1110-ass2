3d2d34585ab646f005df024e40d13da4b410f09c
Your Name <you@example.com>
2019-10-17 04:38:59 +1100

    Merge branch 'master' of https://gitlab.cecs.anu.edu.au/u6420804/comp1110-ass2-wed13j
    # Conflicts:
    #	game.jar

--- src/comp1110/ass2/vb.java
+++ src/comp1110/ass2/vb.java
@@ -1,13 +1,19 @@
 package comp1110.ass2;
 
+import com.sun.source.tree.Tree;
+import org.w3c.dom.Node;
+
+import javax.swing.tree.TreeNode;
 import java.awt.*;
 import java.util.ArrayList;
 
-public class vb {
+public class vb<T> {
+    public void sb(){
+
+
+    }
     public static void main(String[] args) {
-        Point p = new Point(1,2);
-        ArrayList<Point> points = new ArrayList<>(10);
-        points.add(new Point(1,2));
-        System.out.println(points.contains(p));
+
+
     }
 }
