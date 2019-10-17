1d3a8944f7886d1ae18fd55f379b580525260d62
Your Name <you@example.com>
2019-10-18 06:27:14 +1100

    Merge branch 'master' of https://gitlab.cecs.anu.edu.au/u6420804/comp1110-ass2-wed13j
    # Conflicts:
    #	game.jar

--- src/comp1110/ass2/TreeNode.java
+++ src/comp1110/ass2/TreeNode.java
@@ -3,6 +3,10 @@ package comp1110.ass2;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+/**
+ * The author of this class is the entire group, with
+ * inspiration from: https://stackoverflow.com/questions/3522454/java-tree-data-structure
+ */
 public class TreeNode {
     String placement;
     TreeNode parent;
@@ -17,26 +21,6 @@ public class TreeNode {
     public void addChild(TreeNode child){
         this.children.add(child);
         child.parent = this;
-    }
-
-    public String toString() {
-        StringBuilder buffer = new StringBuilder(50);
-        print(buffer, "", "");
-        return buffer.toString();
-    }
-
-    private void print(StringBuilder buffer, String prefix, String childrenPrefix) {
-        buffer.append(prefix);
-        buffer.append(placement);
-        buffer.append('\n');
-        for (Iterator<TreeNode> it = children.iterator(); it.hasNext();) {
-            TreeNode next = it.next();
-            if (it.hasNext()) {
-                next.print(buffer, childrenPrefix + "├── ", childrenPrefix + "│   ");
-            } else {
-                next.print(buffer, childrenPrefix + "└── ", childrenPrefix + "    ");
-            }
-        }
     }
 
 
